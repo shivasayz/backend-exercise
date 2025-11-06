243bcc3c6c4fcebce55a68cc37636edd3ef3e194
shivasayz <writeforshivaa@gmail.com>
2025-11-06 23:54:29 +0530

    post API added

--- exercise-1/backend/server.js
+++ exercise-1/backend/server.js
@@ -3,6 +3,12 @@ const app = express();
 import cors from "cors";
 app.use(cors());
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded bodies (for form submissions)
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/api", (req, res) => {
   res.send("Welcome to the backend");
 });
@@ -13,6 +19,28 @@ app.get("/greet", (req, res) => {
 
 app.get("/suprise", (req, res) => {
   res.json({ message: "happy birthday yet", from: "SR!" });
+});
+
+// POST API
+app.post("/formDetails", (req, res) => {
+  console.log(req.body);
+  const { email, name } = req.body;
+
+  if (!email || !email.includes("@")) {
+    return res.status(400).json({ error: "invalid email" });
+  }
+
+  if (!name || typeof name !== "string") {
+    return res
+      .status(400)
+      .json({ status: "failed", error: "name must be string" });
+  }
+
+  res.status(200).json({
+    status: "success",
+    message: "details submitted successfully",
+    data: req.body,
+  });
 });
 
 const PORT = 3000;
--- exercise-1/frontend/src/App.tsx
+++ exercise-1/frontend/src/App.tsx
@@ -4,6 +4,10 @@ import axios from "axios";
 function App() {
   const [welcomeMessage, setWelcomeMessage] = useState("");
   const [greetMessage, setGreetMessage] = useState("");
+  const [formData, setformData] = useState({
+    email: "",
+    name: "",
+  });
 
   const handleClick = async () => {
     const res = await axios.get("http://localhost:3000/greet");
@@ -20,11 +24,65 @@ function App() {
       .catch((error) => console.error("Error:", error));
   }, []);
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setformData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!formData.email.trim() || !formData.name.trim()) {
+      alert("Please fill in all fields");
+      return;
+    }
+
+    console.log(formData);
+  };
+
   return (
     <>
       <h1>Hello, {welcomeMessage}</h1>
       <button onClick={handleClick}>Click to respond</button>
       {greetMessage && <h3>Greet Message: {greetMessage}</h3>}
+
+      <br />
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-col mt-10">
+          <div className="mt-4">
+            <input
+              type="email"
+              name="email"
+              className="bg-white pt-2 pb-2 px-4 rounded-xl text-2xl text-blue-600 font-sans"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="mt-4">
+            <input
+              type="name"
+              name="name"
+              className="bg-white pt-2 pb-2 px-4 rounded-xl text-2xl text-blue-600 font-sans"
+              placeholder="enter your name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mt-4">
+            <button className="text-shadow-violet-50">Submit</button>
+          </div>
+        </div>
+
+        <div className="mt-4">
+          <p className="text-2xl text-green-400">Successfully Submitted!</p>
+        </div>
+      </form>
     </>
   );
 }
